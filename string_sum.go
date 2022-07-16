5363768312119c40f6bee558cf0a8b78f2ddd5d1
IlyaGrushevsky <96526822+IlyaGrushevsky@users.noreply.github.com>
2022-07-16 12:00:29 +0300

    Add files via upload

--- string_sum.go
+++ string_sum.go
@@ -1,29 +1,43 @@
 package string_sum
 
 import (
-	//"errors"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
-/*var (
+var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-)*/
+)
 
 func StringSum(input string) (output string, err error) {
 	var sum int = 0
+	var count int = 0
+	if _, e := strconv.ParseInt(input, 10, 64); e != nil {
+		err := fmt.Errorf("Пустой ввод %w", errorEmptyInput)
+		fmt.Println(err.Error())
+		return "", err
+	}
 	input1 := strings.Split(strings.TrimSpace(strings.Replace(input, "-", "+-", -1)), "+")
 	for _, i := range input1 {
-		a, err := strconv.Atoi(i)
+		a, e := strconv.Atoi(i)
 		sum += a
+		count++
+		err := fmt.Errorf("Некорректный ввод %w", e)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println(err.Error())
+			return "", err
 		}
+	}
+	if count != 2 {
+		err := fmt.Errorf("Пустой ввод %w", errorNotTwoOperands)
+		fmt.Println(err.Error())
+		return "", err
 	}
 	output = strconv.Itoa(sum)
 
